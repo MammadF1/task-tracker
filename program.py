c5509ae440dacf5b292d7238c14439e7c52722d3
mohammad ali <mohammadaliaghlmandf1@gmail.com>
2025-09-16 16:08:28 +0330

    added delete functionality

--- program.py
+++ program.py
@@ -35,32 +35,44 @@ def list_tasks():
     except Exception as e:
         print("No tasks found.")
     
-def update_task(id_num):
+def update_task(id_num, new_status):
     try:
         with open("tasks.json", "r") as file:
             tasks = json.load(file)
             for task in tasks:
                 if task["id_number"] == id_num:
-                    task["status"] = "done"
+                    task["status"] = new_status
                     break
         with open("tasks.json", "w") as file:
             json.dump(tasks, file, indent=4)
     except Exception as e:
         print("Error updating task:", e)
+def delete_task(id_num):
+    try:
+        with open("tasks.json", "r") as file:
+            tasks = json.load(file)
+            tasks = [task for task in tasks if task["id_number"] != id_num]
+        with open("tasks.json", "w" ) as file:
+            json.dump(tasks, file, indent=4)
+            print(f"Task {id_num} deleted successfully.")
+    except Exception as e:
+        print("Error deleting task:", e)
+
 
 
 
 option = 0
 
-while option != "4":
+while option != "5":
 
     print("1. Add Task")
     print("2. List Tasks")
     print("3. Update Task")
-    print("4. Exit")
+    print("4. Delete Task")
+    print("5. Exit")
 
     option = input("select an option:")
-    if option == "4":
+    if option == "5":
         print("Exiting...")
         break
 
@@ -80,5 +92,10 @@ while option != "4":
         list_tasks()
     if option == "3":
         update_id = int(input("Enter task ID to update: "))
-        update_task(update_id)
-        print(f"Task {update_id} was marked as done!")
+        new_status = input("Enter new status (to do/in progress/done): ")
+        update_task(update_id, new_status)
+        print(f"Task {update_id} was updated to '{new_status}'!")
+    if option == "4":
+        delete_id = int(input("Enter task ID to delete: "))
+        delete_task(delete_id)
+        print(f"Task {delete_id} was deleted successfully!")
